05e3c3ff57c8cc73b4b7ab553a6ab721d832253c
Eric Reichert <eric@reichertconsulting.com>
2023-08-06 08:31:19 -0400

    Added todos.

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ def get_files(path):
 
     for file_path in file_paths:
         print(file_path)
-
+    
+    # TODO turn this into a debugging statement
     print(len(file_paths))
 
 def main():
+    # TODO This should be a command line argument
     path = "/Users/ereichert/Library/CloudStorage/SynologyDrive-household/My Pictures"
     get_files(path)
 
