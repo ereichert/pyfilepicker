505c4666a4cf3c1ae153e8c7629c45a8ab766910
Eric Reichert <eric@reichertconsulting.com>
2023-08-06 10:30:30 -0400

    Added choosing random files.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 EXTENSIONS_OF_INTEREST = ['JPG', 'jpg', 'jpeg','png', 'tif', 'gif', 'TIF']
 
@@ -17,17 +18,33 @@ def get_files(path):
             if possible_extension in EXTENSIONS_OF_INTEREST:
                 file_paths.append(os.path.join(root, file))
 
-    for file_path in file_paths:
-        print(file_path)
+    # TODO Turn this into a debugging statement or command line option
+    # for file_path in file_paths:
+    #     print(file_path)
     
     # TODO turn this into a debugging statement
-    print(len(file_paths))
-    print(extensions)
+    # print(len(file_paths))
+    # TODO Put this behind a command line option
+    # print(extensions)
 
+    return file_paths
+
+def get_random_file_paths(starting_file_paths, number_of_files = 50):
+    files_of_interest = []
+    for _ in range(number_of_files):
+        random_num = random.randrange(0, len(starting_file_paths) + 1)
+        files_of_interest.append(starting_file_paths[random_num])
+
+    # TODO Put this behind a command line argument or debugging statement
+    # for file_path in files_of_interest:
+    #     print(file_path)
+    return files_of_interest
+        
 def main():
     # TODO This should be a command line argument
     path = "/Users/ereichert/Library/CloudStorage/SynologyDrive-household/My Pictures"
-    get_files(path)
+    file_paths = get_files(path)
+    random_file_paths = get_random_file_paths(file_paths)
 
 
 if __name__ == '__main__':
